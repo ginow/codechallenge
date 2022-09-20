e5cadd837bb307ff10aebb2b7115438541ba85c9
Gino <gino.jameskutty@gmail.com>
2022-09-20 09:29:31 +0530

    C build fixed

--- challenge.c
+++ challenge.c
@@ -1,27 +1,50 @@
 #include <stdio.h>
 #include <string.h>
+void cutSticks(int *a, int N, int H)
+{
+    for (int i = 0; i < N; ++i)
+    {
+        a[i] = a[i] - H;
+    }
+}
+int getMax(int *a, int N)
+{
+    int max = 0;
+    for (int i = 0; i < N; ++i)
+    {
+        if (a[i] > max)
+        {
+            max = a[i];
+        }
+    }
+    return max;
+}
 int main()
 {
-    // Read the number of test cases.
-    int T, K, N;
+    int T, N, i = 0, count = 1;
     scanf("%d", &T);
     while (T--)
     {
-        scanf("%d %d", &N, &K);
-        if (K == 0)
+        scanf("%d", &N);
+        int A[N], max = 0;
+        while (i < N)
         {
-            if (N % 4 == 0)
-                printf("Off\n");
-            else
-                printf("On\n");
+            scanf("%d", &A[i]);
+            if (A[i] > max)
+            {
+                max = A[i];
+            }
+            i++;
         }
-        else
+        int *p = A;
+        max--;
+        while (max != 0)
         {
-            if (N % 4 == 0)
-                printf("On\n");
-            else
-                printf("Ambiguous\n");
+            cutSticks(p, N, max);
+            max = getMax(p, N);
+            count++;
         }
+        printf("%d", count);
     }
     return 0;
 }
